aaf1f04eac680cc34e6b7679eb70c033a10d4b72
webdevcc <ze.rav3n@gmail.com>
2024-09-12 21:46:38 -0400

    Gradient

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import "../App.css"
+import React from "react";
+import "../App.css";
+import { LinearGradient } from "react-text-gradients";
+import { RadialGradient } from "react-text-gradients";
+import FancyText from '@carefully-coded/react-text-gradient';
 
 function Intro() {
   return (
-    <div id="intro" className='intro'>
-      <h1 data-aos="fade-up">WEBDEVCC</h1>
-      <p data-aos="fade-up">We create Websites for start ups and established businesses in North America.</p>
+    <div id="intro" className="intro">
+      <h1>
+        <LinearGradient gradient={["to left", "#17acff ,#ff68f0"]}>
+          WEBDEVCC
+        </LinearGradient>
+      </h1>
+      <p data-aos="fade-up">
+        
+      <FancyText
+      gradient={{ from: '#F858E0', to: '#77156C', type: 'linear' }}
+      animateTo={{ from: '#6DEDD0', to: '#7AE23A' }}
+      animateDuration={2000}
+    >
+      We create Websites for start ups and established businesses in North America.
+    </FancyText>
+      </p>
     </div>
   );
 }
